3bb49c4cca59a1b5a6b75d600f45643b20072bed
Devin Ryan <devin.ryan7@gmail.com>
2017-11-28 10:17:34 -0600

    fixed popup to work on all artists

--- artistPopUp.js
+++ artistPopUp.js
@@ -1,23 +1,33 @@
-// var embedRichardAlfaro = "<iframe width="100%" height="450" scrolling="no" frameborder="no" src="https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/playlists/322915078&amp;color=%23ff5500&amp;auto_play=false&amp;hide_related=false&amp;show_comments=true&amp;show_user=true&amp;show_reposts=false&amp;show_teaser=true"></iframe>"
-// var embedPbs73 = "<iframe width="100%" height="450" scrolling="no" frameborder="no" src="https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/playlists/339535455&amp;color=%23ff5500&amp;auto_play=false&amp;hide_related=false&amp;show_comments=true&amp;show_user=true&amp;show_reposts=false&amp;show_teaser=true"></iframe>"
-// var embedTainsus = "<iframe width="100%" height="450" scrolling="no" frameborder="no" src="https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/playlists/357679449&amp;color=%23ff5500&amp;auto_play=false&amp;hide_related=false&amp;show_comments=true&amp;show_user=true&amp;show_reposts=false&amp;show_teaser=true"></iframe>"
+// RichardAlfaro = "<iframe width="100%" height="450" scrolling="no" frameborder="no" src="https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/playlists/322915078&amp;color=%23ff5500&amp;auto_play=false&amp;hide_related=false&amp;show_comments=true&amp;show_user=true&amp;show_reposts=false&amp;show_teaser=true"></iframe>"
+// Pbs73 = "<iframe width="100%" height="450" scrolling="no" frameborder="no" src="https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/playlists/339535455&amp;color=%23ff5500&amp;auto_play=false&amp;hide_related=false&amp;show_comments=true&amp;show_user=true&amp;show_reposts=false&amp;show_teaser=true"></iframe>"
+// Tainsus = "<iframe width="100%" height="450" scrolling="no" frameborder="no" src="https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/playlists/357679449&amp;color=%23ff5500&amp;auto_play=false&amp;hide_related=false&amp;show_comments=true&amp;show_user=true&amp;show_reposts=false&amp;show_teaser=true"></iframe>"
+// Pi Wrecks = <iframe width="100%" height="166" scrolling="no" frameborder="no" src="https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/331346664&amp;color=%23ff5500&amp;auto_play=false&amp;hide_related=false&amp;show_comments=true&amp;show_user=true&amp;show_reposts=false&amp;show_teaser=true"></iframe>
+// bvsmv = <iframe width="100%" height="166" scrolling="no" frameborder="no" src="https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/348223513&amp;color=%23ff5500&amp;auto_play=false&amp;hide_related=false&amp;show_comments=true&amp;show_user=true&amp;show_reposts=false&amp;show_teaser=true"></iframe>
+// ac-tone = <iframe width="100%" height="166" scrolling="no" frameborder="no" src="https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/315708084&amp;color=%23ff5500&amp;auto_play=false&amp;hide_related=false&amp;show_comments=true&amp;show_user=true&amp;show_reposts=false&amp;show_teaser=true"></iframe>
+// midday static = <iframe width="100%" height="450" scrolling="no" frameborder="no" src="https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/playlists/330890554&amp;color=%23ff5500&amp;auto_play=false&amp;hide_related=false&amp;show_comments=true&amp;show_user=true&amp;show_reposts=false&amp;show_teaser=true"></iframe>
+// all buttons in = <iframe width="100%" height="166" scrolling="no" frameborder="no" src="https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/359574692&amp;color=%23ff5500&amp;auto_play=false&amp;hide_related=false&amp;show_comments=true&amp;show_user=true&amp;show_reposts=false&amp;show_teaser=true"></iframe>
+// polsteam baluga = <iframe width="100%" height="166" scrolling="no" frameborder="no" src="https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/339523637&amp;color=%23ff5500&amp;auto_play=false&amp;hide_related=false&amp;show_comments=true&amp;show_user=true&amp;show_reposts=false&amp;show_teaser=true"></iframe>
+// cesium swimsuit = <iframe width="100%" height="166" scrolling="no" frameborder="no" src="https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/337311406&amp;color=%23ff5500&amp;auto_play=false&amp;hide_related=false&amp;show_comments=true&amp;show_user=true&amp;show_reposts=false&amp;show_teaser=true"></iframe>
 
 
+var artistArray = document.querySelectorAll('.name');
 
-
-var artistEl = document.querySelector('h5');
 // add event listener to h5 elements
-artistEl.addEventListener('click', e => {
-  e.preventDefault();
-  // get sibling iframe
-  var iframe = e.target.nextElementSibling;
-  // remove class of none from that iframe
-  iframe.classList.remove("none");
-  // create a button within #artist div that pops up with soundCloud player
-    var rmvBtn = document.createElement('button', className="rmvBtn");
+for (var i = 0; i < artistArray.length; i++) {
+  var artistEl = artistArray[i];
+  artistEl.addEventListener('click', e => {
+    e.preventDefault();
+    // get sibling iframe
+    var iframe = e.target.nextElementSibling;
+    // remove class of none from that iframe
+    iframe.classList.remove("none");
+    // create a button within #artist div that pops up with soundCloud player
+    var rmvBtn = document.createElement('button');
+    rmvBtn.className = "rmvbtn";
     rmvBtn.innerHTML = "Close SoundCloud Player";
     // place above player to be visible
     artistEl.parentNode.insertBefore(rmvBtn, artistEl);
+    console.log(artistEl);
     // add an event listener to rmvBtn
       rmvBtn.addEventListener('click', e => {
         e.preventDefault();
@@ -26,7 +36,10 @@ artistEl.addEventListener('click', e => {
         // remove rmvBtn button
         rmvBtn.parentNode.removeChild(rmvBtn);
     });
-});
+  });
+}
+
+
 
 
 
