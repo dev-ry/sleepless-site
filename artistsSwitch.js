f052c24aab31c2eb7c8841743646715d49a65dbc
Devin Ryan <devin.ryan7@gmail.com>
2018-01-08 16:08:51 -0600

    for Peter to check out

--- artistsSwitch.js
+++ artistsSwitch.js
@@ -1,5 +1,3 @@
-
-// make variables of all artists iframes
 var devras = '<iframe width="100%" height="166" scrolling="no" frameborder="no" src="https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/366820397&amp;color=%23ff5500&amp;auto_play=false&amp;hide_related=false&amp;show_comments=true&amp;show_user=true&amp;show_reposts=false&amp;show_teaser=true"></iframe>';
 var richardAlfaro = '<iframe class="none" width="100%" height="450" scrolling="no" frameborder="no" src="https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/playlists/322915078&amp;color=%23ff5500&amp;auto_play=false&amp;hide_related=false&amp;show_comments=true&amp;show_user=true&amp;show_reposts=false&amp;show_teaser=true"></iframe>';
 var pbs73 = '<iframe class="none" width="100%" height="450" scrolling="no" frameborder="no" src="https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/playlists/339535455&amp;color=%23ff5500&amp;auto_play=false&amp;hide_related=false&amp;show_comments=true&amp;show_user=true&amp;show_reposts=false&amp;show_teaser=true"></iframe>';
@@ -12,33 +10,26 @@ var allButtonsIn = '<iframe width="100%" height="166" scrolling="no" frameborder
 var polsteamBeluga = '<iframe width="100%" height="166" scrolling="no" frameborder="no" src="https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/339523637&amp;color=%23ff5500&amp;auto_play=false&amp;hide_related=false&amp;show_comments=true&amp;show_user=true&amp;show_reposts=false&amp;show_teaser=true"></iframe>';
 var cesiumSwimsuit = '<iframe width="100%" height="166" scrolling="no" frameborder="no" src="https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/337311406&amp;color=%23ff5500&amp;auto_play=false&amp;hide_related=false&amp;show_comments=true&amp;show_user=true&amp;show_reposts=false&amp;show_teaser=true"></iframe>';
 
-var artistsArray = [richardAlfaro, pbs73, tainsus, piWrecks, bvsmv, acTone, middayStatic, allButtonsIn, polsteamBeluga, cesiumSwimsuit];
-
-var musicPlaying = false;
 
 var body = document.querySelector('body');
 
-function addMusic(event) {
-  document.getElementById('richardAlfaro').innerHTML = richardAlfaro;
-  musicPlaying = true;
-  // document.querySelector('.main-wrap').style.backgroundColor = 'black';
-  body.classList.add('blur-out');
+function addMusic () {
+  var artistPlaying;
+  switch (artistPlaying) {
+    case richardAlfaro:
+      document.getElementById('richardAlfaro').innerHTML = richardAlfaro;
+      musicPlaying = true;
+      body.classList.add('blur-out');
+      Break;
+    case pbs73:
+      document.getElementById('pbs73').innerHTML = pbs73;
+      musicPlaying = true;
+      body.classList.add('blur-out');
+      Break;
+    case tainsus:
+      document.getElementById('tainsus').innerHTML = tainsus;
+      musicPlaying = true;
+      body.classList.add('blur-out');
+      Break;
+  }
 }
-
-function removeMusic(event) {
-  document.getElementById('richardAlfaro').innerHTML = "";
-  musicPlaying = false;
-  // document.querySelector('.main-wrap').style.backgroundColor = '#e7e7e7';
-  body.classList.remove('blur-out');
-}
-
- function toggleNav() {
-      musicPlaying ? removeMusic() : addMusic();
-    }
-
-document.querySelector('.text').onclick = toggleNav;
-
-//////////////////////////////////////////////////////////////////////
-
-
-
